dba670cd41d6e801ff50d54ffbe89becfbeb1988
joe101-cmyk <yousiefnasar@gmail.com>
2026-04-25 15:06:43 +0300

    describe your update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 class User {
+    public notebooks: Notebook[] = [];
+
     constructor(
         public id: number,
         public name: string,
@@ -12,6 +14,10 @@ class User {
         console.log(`Id: ${this.id}`);
         console.log(`Name: ${this.name}`);
         console.log(`Email: ${this.email}`);
+    }
+
+    addNotebook(notebook: Notebook): void {
+        this.notebooks.push(notebook);
     }
 }
 
@@ -49,5 +55,41 @@ class Note {
 
     preview(): string {
         return this.content.substring(0, 30) + "------";
+    }
+}
+
+class Notebook {
+    private notes: Note[] = [];
+
+    constructor(public id: number, public name: string) {}
+
+    addNote(note: Note): void {
+        this.notes.push(note);
+        console.log("Note added successfully");
+    }
+
+    removeNote(noteId: number): void {
+        this.notes = this.notes.filter(note => note.id !== noteId);
+        console.log(`Note with id ${noteId} removed`);
+    }
+
+    getAllNotes(): Note[] {
+        return this.notes;
+    }
+}
+
+class Storage<T> {
+    private items: T[] = [];
+
+    addItem(item: T): void {
+        this.items.push(item);
+    }
+
+    removeItem(index: number): void {
+        this.items.splice(index, 1);
+    }
+
+    getAllItems(): T[] {
+        return this.items;
     }
 }
